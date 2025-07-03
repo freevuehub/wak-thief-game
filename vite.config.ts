5a17dd714445846bac4afa2be705b790ba025192
freevuehub <hackish113@gamil.com>
2025-07-03 22:16:49 +0900

    feat: prettier 설정 파일 추가 및 package.json, pnpm-lock.yaml에 의존성 추가; tsconfig.json의 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
-import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, ".", "");
+  const env = loadEnv(mode, '.', '')
+
   return {
     define: {
-      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      __GOOGLE_PRIVATE_KEY__: JSON.stringify(env.GOOGLE_PRIVATE_KEY),
+      __GOOGLE_CLIENT_EMAIL__: JSON.stringify(env.GOOGLE_CLIENT_EMAIL),
+      __GOOGLE_CLIENT_ID__: JSON.stringify(env.GOOGLE_CLIENT_ID),
+      __GOOGLE_API_KEY__: JSON.stringify(env.GOOGLE_API_KEY),
     },
     preview: {
       port: 18777,
-      allowedHosts: ["localhost", "127.0.0.1", "0.0.0.0", "thief.freevue.dev"],
+      allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'thief.freevue.dev'],
     },
     server: {
       port: 18777,
-      allowedHosts: ["localhost", "127.0.0.1", "0.0.0.0", "thief.freevue.dev"],
+      allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'thief.freevue.dev'],
     },
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "."),
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  };
-});
+  }
+})
