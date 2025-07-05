5fc929e4042f859a6b3a29a5277ac40e815a5b23
freevuehub <hackish113@gamil.com>
2025-07-05 14:10:18 +0900

    feat: AIProvider에서 AI 로딩 상태 관리 추가 및 도둑 생성, 이미지 생성, 퇴출, 휴식 기능에 로딩 상태 반영

--- src/provider/AI.tsx
+++ src/provider/AI.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 import { thiefAI } from '@/lib'
 import { usePrompt } from '@/hooks'
 import { PROMPT_KEY } from '@/constants'
@@ -7,6 +7,7 @@ import { Profile, Thief, ImageParams, ThrowOutThiefParams, RestThiefParams } fro
 import { v4 as uuidv4 } from 'uuid'
 
 export const AIContext = createContext({
+  aiLoading: false,
   createThief: (_: Profile) => new Promise<Thief>(() => {}),
   createThiefImage: (_: ImageParams) => new Promise<string>(() => {}),
   throwOutThief: (_: ThrowOutThiefParams) => new Promise<{ dialogue: Array<string> }>(() => {}),
@@ -19,40 +20,70 @@ type Props = {
 
 const AIProvider: React.FC<Props> = (props) => {
   const { prompt } = usePrompt()
+  const [aiLoading, setAiLoading] = useState(false)
 
   return (
     <AIContext.Provider
       value={{
+        aiLoading,
         createThief: (profile: Profile) => {
-          return new Promise<Thief>((resolve) => {
-            pipe(profile, thiefAI.createThief(prompt[PROMPT_KEY.CREATE_THIEF].ko), (data) => {
-              resolve({ ...data, id: uuidv4() })
-            })
+          return new Promise<Thief>((resolve, reject) => {
+            try {
+              setAiLoading(true)
+              pipe(profile, thiefAI.createThief(prompt[PROMPT_KEY.CREATE_THIEF].ko), (data) => {
+                resolve({ ...data, id: uuidv4() })
+              })
+            } catch (error) {
+              reject(error)
+            } finally {
+              setAiLoading(false)
+            }
           })
         },
         createThiefImage: (params: ImageParams) => {
-          return new Promise<string>((resolve) => {
-            pipe(
-              params,
-              thiefAI.createThiefImage(prompt[PROMPT_KEY.CREATE_PROFILE_IMAGE].ko),
-              (data) => {
-                resolve(data)
-              }
-            )
+          return new Promise<string>((resolve, reject) => {
+            try {
+              setAiLoading(true)
+              pipe(
+                params,
+                thiefAI.createThiefImage(prompt[PROMPT_KEY.CREATE_PROFILE_IMAGE].ko),
+                (data) => {
+                  resolve(data)
+                }
+              )
+            } catch (error) {
+              reject(error)
+            } finally {
+              setAiLoading(false)
+            }
           })
         },
         throwOutThief: (params: ThrowOutThiefParams) => {
-          return new Promise<{ dialogue: Array<string> }>((resolve) => {
-            pipe(params, thiefAI.throwOutThief(prompt[PROMPT_KEY.THROW_OUT_THIEF].ko), (data) => {
-              resolve(data)
-            })
+          return new Promise<{ dialogue: Array<string> }>((resolve, reject) => {
+            try {
+              setAiLoading(true)
+              pipe(params, thiefAI.throwOutThief(prompt[PROMPT_KEY.THROW_OUT_THIEF].ko), (data) => {
+                resolve(data)
+              })
+            } catch (error) {
+              reject(error)
+            } finally {
+              setAiLoading(false)
+            }
           })
         },
         restThief: (params: RestThiefParams) => {
-          return new Promise<{ dialogue: Array<string> }>((resolve) => {
-            pipe(params, thiefAI.restThief(prompt[PROMPT_KEY.REST_THIEF].ko), (data) => {
-              resolve(data)
-            })
+          return new Promise<{ dialogue: Array<string> }>((resolve, reject) => {
+            try {
+              setAiLoading(true)
+              pipe(params, thiefAI.restThief(prompt[PROMPT_KEY.REST_THIEF].ko), (data) => {
+                resolve(data)
+              })
+            } catch (error) {
+              reject(error)
+            } finally {
+              setAiLoading(false)
+            }
           })
         },
       }}
